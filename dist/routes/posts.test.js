12988de117396b4b9c7df4cd2c180008bd68f363
davidspiv <daspivack@gmail.com>
2024-06-06 15:45:53 -0700

    further refined test

--- dist/routes/posts.test.js
+++ dist/routes/posts.test.js
@@ -1,51 +1,38 @@
-import { expect, test } from 'vitest';
+import { test } from 'vitest';
 import fetch from 'node-fetch';
-const res = await getData();
-const verify = (value) => {
-    return {
-        isTruthy: !!value,
-        isArray: Array.isArray(value),
-        hasSevenKeys: Array.isArray(value)
-            ? Object.keys(value[0]).length === 7
-            : false,
-        hasTransDate: (Array.isArray(value) ? 'trans_date' in value[0] : false) &&
-            typeof value[0].trans_date_offset === 'number',
-        hasTransDateOffset: (Array.isArray(value) ? 'trans_date_offset' in value[0] : false) &&
-            typeof value[0].trans_date_offset === 'number',
-        hasTransAmount: (Array.isArray(value) ? 'trans_amount' in value[0] : false) &&
-            typeof value[0].trans_amount === 'number',
-        hasTransMemo: (Array.isArray(value) ? 'trans_memo' in value[0] : false) &&
-            typeof value[0].acc_id === 'number',
-        hasUserId: (Array.isArray(value) ? 'user_id' in value[0] : false) &&
-            typeof value[0].user_id === 'string',
-    };
-};
-test('Req body formatted correctly', async () => {
-    const { isTruthy, isArray, hasTransDate, hasTransDateOffset, hasTransAmount, hasTransMemo, hasUserId, } = verify(res);
-    if (!isTruthy)
-        throw Error('isTruthy failed');
-    if (!isArray)
-        throw Error('isArray failed');
-    if (!hasTransDate)
-        throw Error('hasTransDate failed');
-    if (!hasTransDateOffset)
-        throw Error('hasTransDateOffset failed');
-    if (!hasTransAmount)
-        throw Error('hasTransAmount failed');
-    if (!hasTransMemo)
-        throw Error('hasTransMemo failed');
-    if (!hasUserId)
-        throw Error('hasUserId failed');
-    expect(isTruthy &&
-        isArray &&
-        hasTransDate &&
-        hasTransDateOffset &&
-        hasTransAmount &&
-        hasTransMemo &&
-        hasUserId).toBe(true);
-});
-async function getData() {
-    const response = await fetch('http://localhost:5000/api/posts/');
+async function getData(address) {
+    const response = await fetch(address);
     const data = await response.json();
     return data;
 }
+test('@route GET /api/posts: res formatted correctly', async () => {
+    const resBody = await getData('http://localhost:5000/api/posts/');
+    const isArray = Array.isArray(resBody);
+    if (!isArray)
+        throw Error('@res.body is not an array.');
+    for (let i = 0; i < resBody.length; i++) {
+        const hasSevenKeys = Object.keys(resBody[i]).length === 7;
+        const date = 'trans_date' in resBody[i] &&
+            typeof resBody[i].trans_date === 'string';
+        const dateOffset = 'trans_date_offset' in resBody[i] &&
+            typeof resBody[i].trans_date_offset === 'number';
+        const amount = 'trans_amount' in resBody[i] &&
+            typeof resBody[i].trans_amount === 'number';
+        const memo = 'trans_memo' in resBody[i] &&
+            typeof resBody[i].acc_id === 'number';
+        const userId = 'user_id' in resBody[i] &&
+            typeof resBody[i].user_id === 'string';
+        if (!hasSevenKeys)
+            throw Error(`@res.body elements @index ${i} don't have exactly 7 keys.`);
+        if (!date)
+            throw Error(`@res.body trans_date @index ${i} missing / wrong type.`);
+        if (!dateOffset)
+            throw Error(`@res.body trans_date_offset @index ${i} missing / wrong type.`);
+        if (!amount)
+            throw Error(`@res.body trans_amount @index ${i} missing / wrong type.`);
+        if (!memo)
+            throw Error(`@res.body trans_memo @index ${i} missing / wrong type.`);
+        if (!userId)
+            throw Error(`@res.body user_id @index ${i} missing / wrong type.`);
+    }
+});
