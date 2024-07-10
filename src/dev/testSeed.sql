f2d587bf15b97b2271bb57455e02f01157b731d2
davidspiv <daspivack@gmail.com>
2024-07-10 15:39:30 -0700

    refactor ok

---
+++ src/dev/testSeed.sql
@@ -0,0 +1,23 @@
+-- User Crandice Seed
+
+INSERT INTO user (usr_name, usr_password, usr_email)
+    VALUES
+    ('Crandice', 'crandice123', 'crandice@gmail.com');
+
+INSERT INTO account (acc_id, acc_name, acc_initial_bal, usr_id)
+    VALUES
+    (1001, 'Schools Checking', 20, 1),
+    (1002, 'Schools Savings', 20, 1),
+    (1003, 'Petty Cash', 20, 1),
+    (1004, 'Receivable', 20, 1),
+    (1005, 'Prepaid', 20, 1),
+    (2001, 'Payable', 20, 1),
+    (3001, 'Stock', 20, 1),
+    (5001, 'Housing', 20, 1),
+    (5002, 'Food', 20, 1),
+    (5003, 'Utilities', 20, 1),
+    (5004, 'Transportation', 20, 1),
+    (5005, 'Household', 20, 1),
+    (5006, 'Education', 20, 1),
+    (5007, 'Gifts', 20, 1),
+    (5008, 'Personal', 20, 1);
