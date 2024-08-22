7428da5a37386e49bd9ee03d0619262cfd9c0cb9
davidspiv <daspivack@gmail.com>
2024-08-21 20:57:13 -0700

    cleaned up addMany controller and model

--- src/controllers/reference/addMany.ts
+++ src/controllers/reference/addMany.ts
@@ -1,40 +1,38 @@
-import { insertModels } from '../../dev/utilDb.js';
-import dynamicQueries from '../../dev/dynamicQueries.js';
-import Database from 'better-sqlite3';
+import {
+	getSourceId,
+	removeDuplicateRefs,
+	insertRefs,
+} from '../../models/reference/addMany.js';
 
 import type { Request, Response, NextFunction } from 'express';
-import type { ReferenceData } from '../../interfaces.js';
-import { addMany } from '../../models/reference/addMany.js';
-
-const getSourceId = () => {
-	const selectStatement = `
-		SELECT *
-		FROM sources;
-		`;
-
-	const db = new Database('accounting.db', {
-		fileMustExist: true,
-		readonly: true,
-	});
-
-	const result = <ReferenceData[]>db.prepare(selectStatement).all();
-
-	db.close();
-	return result[0].src_id;
-};
 
 //@route POST /api/references/
 export default (req: Request, res: Response, next: NextFunction) => {
-	if (typeof req.body !== 'object' || !req.body || !('references' in req.body))
+	if (typeof req.body !== 'object' || !req.body || !('references' in req.body)) {
 		return next(
-			new Error("@res.body is not an object or doesn't have references key."),
+			new Error("[REQUEST BODY] Not an object or doesn't have references key."),
 		);
+	}
 
 	const refArr = req.body.references;
 	const isArray = Array.isArray(refArr);
 
-	if (!isArray) return next(new Error('@req.references is not an array.'));
-	const message = addMany(refArr);
+	if (!isArray) {
+		return next(new Error('[REQUEST BODY] References value is not an array.'));
+	}
 
-	res.status(200).json(message);
+	const sourceId = getSourceId();
+	const newRefs = removeDuplicateRefs(sourceId, refArr);
+
+	if (!newRefs.length) {
+		return res.status(200).json('No new references to input');
+	}
+
+	try {
+		const refIds = insertRefs(sourceId, newRefs);
+
+		res.status(200).json({ refIds });
+	} catch (error) {
+		res.status(500).json(error);
+	}
 };
--- src/models/reference/addMany.ts
+++ src/models/reference/addMany.ts
@@ -1,11 +1,8 @@
-import { insertModels } from '../../dev/utilDb.js';
 import Database from 'better-sqlite3';
+import { randomUUID } from 'node:crypto';
 import dynamicQueries from '../../dev/dynamicQueries.js';
 
-import type {
-	ReferenceData,
-	ReferenceInput,
-} from '../../interfaces.js';
+import type { ReferenceData, ReferenceInput } from '../../interfaces.js';
 
 const readLatest = (srcId: string) => {
 	const selectStatement = `
@@ -27,27 +24,6 @@ const readLatest = (srcId: string) => {
 	return result;
 };
 
-const sortByDate = (refArr: ReferenceInput[]) => {
-	const filterDate = (date: string) => {
-		return new Date(Number.parseInt(date)).getTime();
-	};
-
-	return refArr.sort(
-		(a: ReferenceInput, b: ReferenceInput) =>
-			filterDate(b.date) - filterDate(a.date),
-	);
-};
-
-const getSliceIndex = (recentDate: string, refArr: ReferenceInput[]) => {
-	for (let i = 0; i < refArr.length; i++) {
-		if (refArr[i].date === recentDate) {
-			return i;
-		}
-	}
-
-	return refArr.length;
-};
-
 const getSourceId = () => {
 	const selectStatement = `
 		SELECT *
@@ -65,31 +41,58 @@ const getSourceId = () => {
 	return result[0].src_id;
 };
 
-const addMany = (refArr: ReferenceInput[]): string => {
-	const srcId = getSourceId();
+const sortByDate = (refArr: ReferenceInput[]) => {
+	const filterDate = (date: string) => {
+		return new Date(Number.parseInt(date)).getTime();
+	};
+
+	return refArr.sort(
+		(a: ReferenceInput, b: ReferenceInput) =>
+			filterDate(b.date) - filterDate(a.date),
+	);
+};
+
+const getSliceIndex = (recentDate: string, refArr: ReferenceInput[]) => {
+	for (let i = 0; i < refArr.length; i++) {
+		if (refArr[i].date <= recentDate) {
+			return i;
+		}
+	}
+
+	return refArr.length;
+};
+
+const removeDuplicateRefs = (srcId: string, refArr: ReferenceInput[]) => {
 	const refLatest = <ReferenceData[]>readLatest(srcId);
 	const latestDate = refLatest[0]?.ref_date;
 
-	const filteredRefArr: ReferenceInput[] = [];
-	if (latestDate) {
-		const sortedRefArr = latestDate ? sortByDate(refArr) : refArr;
-		const sliceIndex = getSliceIndex(latestDate, sortedRefArr);
-		console.log(sliceIndex);
-		filteredRefArr.push(...sortedRefArr.slice(0, sliceIndex));
-	} else {
-		filteredRefArr.push(...refArr);
+	if (!latestDate) {
+		return refArr;
 	}
 
-	const modifiedRefArr = filteredRefArr.map((ref) => ({ ...ref, srcId }));
+	const sortedRefArr = sortByDate(refArr);
+	const sliceIndex = getSliceIndex(latestDate, sortedRefArr);
+	const filteredRefArr = sortedRefArr.slice(0, sliceIndex);
 
-	try {
-		const refIds = insertModels(dynamicQueries.insertRefs, modifiedRefArr);
-
-		console.log(`${refIds.length} references submitted`);
-		return String(refIds);
-	} catch (error) {
-		return String(error);
-	}
+	return filteredRefArr;
 };
 
-export { addMany };
+const insertRefs = (srcId: string, models: ReferenceInput[]) => {
+	const db = new Database('accounting.db');
+	const idArr: string[] = [];
+
+	db.transaction(() => {
+		for (const model of models) {
+			const id = randomUUID();
+			//better-sql-3 will reject a class instance
+			db.prepare(dynamicQueries.insertRefs).run({ ...model, id, srcId });
+			idArr.push(id);
+		}
+	})();
+
+	db.close();
+	console.log(`${models.length} references input successfully.`);
+	return idArr;
+};
+
+export { getSourceId, removeDuplicateRefs, insertRefs };
